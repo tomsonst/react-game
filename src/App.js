23613ca0664a4e4d742f88a0310e20cb2ea3f2fa
tomsonst <40933547+tomsonst@users.noreply.github.com>
2021-02-22 21:38:54 +0300

    feat: add animation on field

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Footer from './Components/Footer/Footer.js';
+import FieldGame from './Components/FieldGame/FieldGame.js';
+import Statistic from './Components/Statistic/Statistic.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Statistic />
+        <FieldGame />
+        <Footer />
+      </div>
+    );
+  }
 }
 
-export default App;
